2b1e215ff17e1fdedb396602d459fff4fc5a1b3d
Hargreaves Marotse <marotseh@gmail.com>
2023-12-23 06:15:05 +0300

    practice controlled elements concept

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { useState } from 'react';
+
 const initialItems = [
   { id: 1, description: 'Passports', quantity: 2, packed: false },
   { id: 2, description: 'Socks', quantity: 12, packed: true },
-  { id: 3, description: 'Charger', quantity: 12, packed: true },
+  { id: 3, description: 'Charger', quantity: 2, packed: true },
 ];
 
 export default function App() {
@@ -20,21 +22,44 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState('');
+
+  const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
+    const newItem = { id: Date.now(), description, quantity, packed: false };
+    console.log(newItem);
+
+    setDescription('');
+    setQuantity(1);
   }
   const numbers = Array.from({ length: 20 }, (_, i) => i + 1);
+
+  function handleInputChange(e) {
+    setDescription(e.target.value);
+  }
+
+  function handleSelectChange(e) {
+    setQuantity(Number(e.target.value));
+  }
+
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select>
+      <select value={quantity} onChange={handleSelectChange}>
         {numbers.map((number) => (
           <option value={number} key={number}>
             {number}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item...." />
+      <input
+        type="text"
+        placeholder="Item...."
+        value={description}
+        onChange={handleInputChange}
+      />
       <button>Add</button>
     </form>
   );
